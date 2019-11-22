09ce6d25858b85894aabb4d6969eca99ea2458dc
Fedor Chegarovsky <fedor.chegarovsky@ringcentral.com>
2019-11-22 17:29:51 +0300

    redundant code removed, start and stop functions;

--- capturer/capturer.cc
+++ capturer/capturer.cc
@@ -8,8 +8,6 @@ using namespace CustomMediaStream;
 using FramePtr = ControllerHolder::FramePtr;
 using Format = VideoFrame::Format;
 using Plane = VideoFrame::Plane;
-//v8::Global<v8::Object> control_handle;
-//CustomMediaStream::VideoFrameCallback* callback;
 
 NAN_METHOD(startCapture) {
   std::cerr << "addCaptureStreamToMediaStream" << info.Length() << std::endl;
@@ -19,29 +17,19 @@ NAN_METHOD(startCapture) {
     Nan::ThrowTypeError("11111 cannot unwrap CustomMediaStream::ControllerHolder");
     return;
   }
-  
 
-  
-  //int i = 1;
-  // for (auto i = 0; i < 10000; i++) {
-    auto frame = ctrl->allocate({0}, Format {100, 100});
-
-    // void * dataY = frame->data(Plane::Y);
-    // memset(dataY, 41, 1000);
+  //for (int i = 0; i < 10; i++) {
+    auto frame = ctrl->allocate(0, Format {100, 100});
+ 
+    void * dataY = frame->data(Plane::Y);
+    memset(dataY, 100, 1000);
     void * dataU = frame->data(Plane::U);
-    memset(dataU, 161, 5000);
+    memset(dataU, 0, 3000);
     void * dataV = frame->data(Plane::V);
-    memset(dataV, 169, 5000);
+    memset(dataV, 0, 5000);
 
-    ctrl->queue({0}, std::move(frame));
-  // }
-
-    //auto frame = ctrl->allocate({0}, Format {100, 100});
-  // for (auto i = 0; i < 10000; i++) {
-  //int i = 1;
+    ctrl->queue(0, std::move(frame));
   //}
-
-  
 }
 
 NAN_MODULE_INIT(custom_capturer_init) {
--- consumer-app/renderer.js
+++ consumer-app/renderer.js
@@ -1,30 +1,41 @@
 const { customMediaStream } = require('electron');
 const capturer = require('capturer/build/Release/capturer.node');
-
 const videoElem = document.getElementById('video-element');
 
-console.log('before createTrack');
+/// attach debugger to that pid
+console.log('pid ' + process.pid);
 
-let trackInfo = {
-    resolution: { width: 1920, height: 1080 },
-    frameRate: 30
-};
+/// call this function from dev console
+function start() {
+    let trackInfo = {
+        resolution: { width: 1920, height: 1080 },
+        frameRate: 30
+    };
+    
+    console.log('before createTrack');
+    let track = customMediaStream.createTrack(
+        trackInfo,
+        (controller) => {
+            console.log('startCapture controller = ' + controller);
+            capturer.startCapture(controller);
+        },
+        () => {
+            console.log('stopCapture');
+            clearInterval(this.interval);
+        }
+    )
 
-let track = customMediaStream.createTrack(
-    trackInfo,
-    (controller) => {
-        console.log('startCapture controller = ' + controller);
-        capturer.startCapture(controller);
-    },
-    () => {
-        console.log('stopCapture');
-        clearInterval(this.interval);
-    }
-)
+    console.log('before MediaStream');
+    let mediaStream = new MediaStream();
+    mediaStream.addTrack(track);
 
-let mediaStream = new MediaStream();
-mediaStream.addTrack(track);
+    console.log('before srcObject');
+    //this is a html5 video element
+    videoElem.srcObject = mediaStream
+    videoElem.play();
+}
 
-//this is a html5 video element
-videoElem.srcObject = mediaStream
-videoElem.play();
+function stop() {
+    videoElem.srcObject = null;
+    //videoElem.stop();
+}
